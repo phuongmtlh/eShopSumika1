76ed44583646d229e22f83d31e2c4e1b21f49385
Phuong <phuongmt.lh@gmail.com>
2021-03-31 22:00:01 +0700

    Sua loi cap nhat entity va them vao

---
+++ eShopSumika.Data/Enums/TransactionStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSumika.Data.Enums
+{
+    public enum TransactionStatus
+    {
+        Success,
+        Failed
+    }
+}
